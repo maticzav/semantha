df730672339563cae1151c31499bc198ba59e115
maticzav <matic.zavadlal@gmail.com>
2019-01-07 12:30:43 +0100

    version, git tests

--- packages/semantha/src/tests/git.test.ts
+++ packages/semantha/src/tests/git.test.ts
@@ -1,5 +1,118 @@
+import * as execa from 'execa'
+
+import { isRefInHistory, isBranchUpToDate, getRemoteHead } from '../git'
+
 describe('git', () => {
-  test('prepareWorkspace versions workspace correctly', async () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  /**
+   * isRefInHistory
+   */
+
+  test('isRefInHistory correctly reports error', async () => {
+    /* Mock */
+
+    const execaMock = jest.spyOn(execa, 'default').mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = isRefInHistory('master')
+
+    /* Tests */
+
+    expect(2).toBe(2)
+  })
+
+  test('isRefInHistory correctly refInHistory', async () => {
+    /* Mock */
+
+    const execaMock = jest.spyOn(execa, 'default').mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = isRefInHistory('master')
+
+    /* Tests */
+
+    expect(2).toBe(2)
+  })
+
+  /**
+   * getRemoteHead
+   */
+
+  test('getRemoteHead correctly reports error', async () => {
+    /* Mock */
+
+    const execaMock = jest.spyOn(execa, 'default').mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = getRemoteHead('master')
+
+    /* Tests */
+
+    expect(2).toBe(2)
+  })
+
+  test('getRemoteHead correctly finds remote head', async () => {
+    /* Mock */
+
+    const execaMock = jest.spyOn(execa, 'default').mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = getRemoteHead('master')
+
+    /* Tests */
+
+    expect(2).toBe(2)
+  })
+
+  /**
+   * isBranchUpToDate
+   */
+  test('isBranchUpToDate correctly logs remoteHead error', async () => {
+    /* Mock */
+
+    const execaMock = jest.spyOn(execa, 'default').mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = isBranchUpToDate('master')
+
+    /* Tests */
+
+    expect(2).toBe(2)
+  })
+
+  test('isBranchUpToDate correctly logs refInHistory error', async () => {
+    /* Mock */
+
+    const execaMock = jest.spyOn(execa, 'default').mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = isBranchUpToDate('master')
+
+    /* Tests */
+
+    expect(2).toBe(2)
+  })
+
+  test('isBranchUpToDate correctly determines whether branch is up to date', async () => {
+    /* Mock */
+
+    const execaMock = jest.spyOn(execa, 'default').mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = isBranchUpToDate('master')
+
+    /* Tests */
+
     expect(2).toBe(2)
   })
 })
--- packages/semantha/src/tests/version.test.ts
+++ packages/semantha/src/tests/version.test.ts
@@ -1,5 +1,71 @@
+import { prepareWorkspace } from '../version'
+
+import * as writePkg from 'write-pkg'
+
 describe('version', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  test('prepareWorkspace correctly reports error', async () => {
+    /* Mock */
+
+    const writePkgMock = jest
+      .spyOn(writePkg, 'default')
+      .mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = await prepareWorkspace(
+      {
+        commits: [],
+        version: 1,
+        workspace: {
+          path: '/packages/package-c',
+          pkg: {
+            name: 'package-c',
+            version: '1.0.0',
+            dependencies: {},
+            devDependencies: {},
+          },
+        },
+      },
+      [],
+    )
+
+    /* Tests */
+
+    expect(2).toBe(2)
+  })
+
   test('prepareWorkspace versions workspace correctly', async () => {
+    /* Mock */
+
+    const writePkgMock = jest
+      .spyOn(writePkg, 'default')
+      .mockResolvedValue(undefined)
+
+    /* Execution */
+
+    const res = await prepareWorkspace(
+      {
+        commits: [],
+        version: 1,
+        workspace: {
+          path: '/packages/package-c',
+          pkg: {
+            name: 'package-c',
+            version: '1.0.0',
+            dependencies: {},
+            devDependencies: {},
+          },
+        },
+      },
+      [],
+    )
+
+    /* Tests */
+
     expect(2).toBe(2)
   })
 })
