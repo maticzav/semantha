0121030aa302316d106953f2ded3368a9f098ecb
maticzav <matic.zavadlal@gmail.com>
2019-01-07 23:48:42 +0100

    tests

--- packages/semantha/src/tests/version.test.ts
+++ packages/semantha/src/tests/version.test.ts
@@ -1,4 +1,9 @@
-// import { prepareWorkspace } from '../version'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import { SemanthaRelease } from 'semantha-core'
+import tempy from 'tempy'
+
+import { prepareWorkspace } from '../version'
 
 import * as writePkg from 'write-pkg'
 
@@ -10,9 +15,9 @@ describe('version', () => {
   test('prepareWorkspace correctly reports error', async () => {
     /* Mock */
 
-    const writePkgMock = jest
-      .spyOn(writePkg, 'default')
-      .mockResolvedValue(undefined)
+    const writePkgMock = jest.spyOn(writePkg, 'sync').mockImplementation(() => {
+      throw new Error('pass')
+    })
 
     /* Execution */
 
@@ -35,37 +40,107 @@ describe('version', () => {
 
     /* Tests */
 
-    expect(2).toBe(2)
+    expect(res).toEqual({
+      status: 'err',
+      message: 'pass',
+    })
+    expect(writePkgMock).toBeCalledWith({
+      version: '1.0.1',
+      dependencies: {},
+      devDependencies: {},
+    })
   })
 
   test('prepareWorkspace versions workspace correctly', async () => {
-    /* Mock */
+    /* Setup */
 
-    const writePkgMock = jest
-      .spyOn(writePkg, 'default')
-      .mockResolvedValue(undefined)
+    const tmp = tempy.directory()
 
-    /* Execution */
+    const w = path.resolve(__dirname, './__fixtures__/workspaces/valid/')
+    fs.copy(w, tmp)
 
-    const res = await prepareWorkspace(
+    const releases: SemanthaRelease[] = [
+      {
+        commits: [],
+        version: 3,
+        workspace: {
+          path: path.resolve(tmp, './packages/package-a/'),
+          pkg: {
+            name: 'package-a',
+            version: '1.0.0',
+            dependencies: {
+              something: '1.0.0',
+              'package-b': '1.0.2',
+            },
+            devDependencies: {
+              devit: '1.0.0',
+              'package-c': '2.0.0',
+            },
+          },
+        },
+      },
+      {
+        commits: [],
+        version: 2,
+        workspace: {
+          path: path.resolve(tmp, './packages/package-b/'),
+          pkg: {
+            name: 'package-b',
+            version: '1.0.2',
+            dependencies: {
+              something: '1.0.0',
+              'package-c': '2.0.0',
+            },
+            devDependencies: {
+              devit: '1.0.0',
+            },
+          },
+        },
+      },
       {
         commits: [],
         version: 1,
         workspace: {
-          path: '/packages/package-c',
+          path: path.resolve(tmp, './packages/package-c/'),
           pkg: {
             name: 'package-c',
-            version: '1.0.0',
-            dependencies: {},
-            devDependencies: {},
+            version: '2.0.0',
+            dependencies: {
+              something: '1.0.0',
+            },
+            devDependencies: {
+              devit: '1.0.0',
+            },
           },
         },
       },
-      [],
+    ]
+
+    /* Execution */
+
+    const res = await prepareWorkspace(releases[0], releases)
+
+    const pkg = JSON.parse(
+      fs.readFileSync(`${releases[0].workspace.path}/package.json`, 'utf-8'),
     )
 
     /* Tests */
 
-    expect(2).toBe(2)
+    expect(res).toEqual({
+      status: 'ok',
+      release: releases[0],
+    })
+    expect(pkg).toEqual({
+      name: 'package-a',
+      version: '2.0.0',
+      dependencies: {
+        something: '1.0.0',
+        'package-b': '1.1.0',
+      },
+      devDependencies: {
+        devit: '1.0.0',
+        'package-c': '2.0.1',
+      },
+    })
   })
 })
